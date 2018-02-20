85edb2f2c6c257578dc4479adea73131df126a82
Clayton <cmusall@pcnclosings.com>
2018-02-20 08:34:01 -0500

    Added signing and signing party models and context

---
+++ SigningService/Models/Signing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SigningService.Models
+{
+    public class Signing
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+        public string FileNumber { get; set; }
+        public DateTime ClosingDateTime { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string MobilePhone { get; set; }
+        public string HomePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string EmailAddress { get; set; }
+        public string ClosingLocation { get; set; }
+        public string ClosingAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string County { get; set; }
+
+        public virtual ICollection<SigningParty> SigningParties { get; set; }
+    }
+}
