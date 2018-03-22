4fd53fc0579cf6d7e4d44493ac7aeb0be7325706
Unknown <cmusall@pcnclosings.com>
2018-03-22 14:49:33 -0400

    Update the validate order result utility message

--- OrderPlacement/Utilities/ValidIncomingOrderUtility.cs
+++ OrderPlacement/Utilities/ValidIncomingOrderUtility.cs
@@ -6,12 +6,12 @@ namespace OrderPlacement.Utilities
     {
         internal ValidIncomingOrderResult IsIncomingOrderDataValid(string fileNumber, OrderPlacementServicePropertyAddress propertyAddress, OrderPlacementServicePartner lender)
         {
-            if (string.IsNullOrWhiteSpace(fileNumber)) return new ValidIncomingOrderResult { Valid = false, Message = "File number is null or empty" };
+            if (string.IsNullOrWhiteSpace(fileNumber)) return new ValidIncomingOrderResult { Valid = false, Message = "File number is null or empty. PCN requires a file number." };
 
-            if (propertyAddress == null) return new ValidIncomingOrderResult { Valid = false, Message = "Property address is null or empty" };
+            if (propertyAddress == null) return new ValidIncomingOrderResult { Valid = false, Message = "Property address is null or empty. PCN requires a property address." };
 
             return lender == null ? 
-                new ValidIncomingOrderResult { Valid = false, Message = "Lender is null or empty" } : 
+                new ValidIncomingOrderResult { Valid = false, Message = "Lender is null or empty. PCN requires a lender." } : 
                 new ValidIncomingOrderResult { Valid = true };
         }
     }
